f11b4eb0bf7d9debef28ae9fccedfba855d6155d
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-05-08 19:03:56 +0300

    Удаление хлама.

--- MailSender.ConsoleTest/Program.cs
+++ MailSender.ConsoleTest/Program.cs
@@ -14,34 +14,7 @@ namespace MailSender.ConsoleTest
     {
         static void Main(string[] args)
         {
-            using (var message = new MailMessage("shmachilin@yandex.ru", "shmachilin@gmail.com"))
-            {
-                message.From = new MailAddress("shmachilin@yandex.ru", "От меня");
-                message.To.Add(new MailAddress("shmachilin@gmail.com", "Ко мне"));
-
-                message.Subject = "Заголовок сообщения";
-                message.Body = "Текст сообщения";
-
-                message.Attachments.Add(new Attachment("FileName.txt"));
-
-                SmtpClient client = null;
-                try
-                {
-                    client = new SmtpClient("mail.yandex.ru", 465);
-                    client.EnableSsl = true;
-                    client.Credentials = new NetworkCredential("UserName", "Password");
-
-                    client.Send(message);
-                }
-                finally
-                {
-                  if(client != null)
-                        client.Dispose();
-                }
-
-            }
-
-            //Console.ReadLine();
+            Console.ReadLine();
         }
     }
 }
