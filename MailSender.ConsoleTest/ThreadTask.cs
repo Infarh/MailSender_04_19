a56807b15435d93de252ba4bcc0329c86e80e512
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-05-22 20:58:28 +0300

    Примеры работы с многопоточностью

---
+++ MailSender.ConsoleTest/ThreadTask.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace MailSender.ConsoleTest
+{
+    internal class ThreadTask
+    {
+        public string Data { get; set; }
+
+        public void ThreadMethod()
+        {
+            var data = Data;
+            for (var i = 0; i < 100_000; i++)
+            {
+                Thread.Sleep(100);
+                Console.WriteLine($"Message({i}): {data} : thread id: {Thread.CurrentThread.ManagedThreadId}");
+            }
+        }
+    }
+}
--- MailSender.WPF.Tests/MainWindow2.xaml.cs
+++ MailSender.WPF.Tests/MainWindow2.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,17 +12,32 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MailSender.WPF.Tests
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow2.xaml
-    /// </summary>
     public partial class MainWindow2 : Window
     {
         public MainWindow2()
         {
             InitializeComponent();
+        }
+
+        private void ThreadStartClick(object Sender, RoutedEventArgs E)
+        {
+            var thread = new Thread(ThreadMethod);
+            thread.Start();
+        }
+
+        private void ThreadMethod()
+        {
+            for (var i = 0; i < 100000; i++)
+            {
+                Thread.Sleep(10);
+                TextValue.Dispatcher.Invoke(() => TextValue.Text = i.ToString());
+                //TextValue.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() => TextValue.Text = i.ToString()));
+                //TextValue.Text = i.ToString();
+            }
         }
     }
 }
