51d121325c4ef3544a9c243fee3cf5bc482e569e
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-05-29 20:02:09 +0300

    Примеры использования класса Parallel из библиотеки TPL

--- MailSender.ConsoleTest/Program.cs
+++ MailSender.ConsoleTest/Program.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using System.Threading.Tasks;
+
 namespace MailSender.ConsoleTest
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //ParallelTest.Start();
+            TaskTest.Start();           
 
+
+            Console.WriteLine("Основной поток завершился. Нажмите Enter для выхода.");
             Console.ReadLine();
         }
     }
---
+++ MailSender.ConsoleTest/TaskTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender.ConsoleTest
+{
+    internal static class TaskTest
+    {
+        public static void Start()
+        {
+
+        }
+    }
+}
