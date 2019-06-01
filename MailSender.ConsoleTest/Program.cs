eaf5b1fd32536d0d7f226961f7f7ace69f016b85
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-06-01 20:21:44 +0300

    EntityFramework + миграции

--- MailSender.ConsoleTest/Program.cs
+++ MailSender.ConsoleTest/Program.cs
@@ -1,16 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-
-using System.Threading.Tasks;
+using MailSender.ConsoleTest.Data;
 
 namespace MailSender.ConsoleTest
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            using (var db = new Data.SongsDB())
+            {
+                foreach (var track in db.Tracks)
+                {
+                    Console.WriteLine("{0}\t{1}\t{2}\t{3}",
+                        track.Id, track.Name, track.ArtistName, track.Length);
+                }
+            }
+
+            Console.WriteLine("-------------------------------");
+
+            using (var db = new SongsDB())
+            {
+                //db.Database.SqlQuery()
+                //db.Database.Log = str => Console.WriteLine("{0}: {1}", DateTime.Now, str);
+
+                foreach (var track in db.Tracks.Where(track => track.ArtistName == "Artist1"))
+                {
+                    Console.WriteLine("{0}\t{1}\t{2}\t{3}",
+                        track.Id, track.Name, track.ArtistName, track.Length);
+                }
+            }
+
+            //using (var db = new SongsDB())
+            //{
+            //    for (var i = 0; i < 1000; i++)
+            //    {
+            //        db.Tracks.Add(new Track
+            //        {
+            //            Name = $"Song-{i}",
+            //            ArtistName = "Test artist",
+            //            Length = 100 * i
+            //        });
+            //    }
+
+            //    db.SaveChanges();
+            //}
+
+            Console.WriteLine("-------------------------------");
+
+            using (var db = new Data.SongsDB())
+            {
+                foreach (var track in db.Tracks)
+                {
+                    Console.WriteLine("{0}\t{1}\t{2}\t{3}",
+                        track.Id, track.Name, track.ArtistName, track.Length);
+                }
+            }
+
             Console.ReadLine();
         }
     }
